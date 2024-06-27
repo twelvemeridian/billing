37f5dd62a5ee89821076d1617f6dcc6df1a52c30
Utkarsh Singh <utkarsh@ISHIRs-MacBook-Pro-2.local>
2024-06-27 07:38:51 +0530

    add loader

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.getElementById("add-item").addEventListener("click", () => {
   itemContainer.innerHTML = `
       <div class="row g-3">
         <div class="col-md-3">
-          <input type="text" class="form-control" placeholder="Item Name" required>
+          <input type="text" class="form-control" placeholder="Item Name*" required>
         </div>
         <div class="col-md-2">
-          <input type="number" class="form-control" placeholder="Quantity" required>
+          <input type="number" class="form-control" placeholder="Quantity*" required>
         </div>
         <div class="col-md-2">
-          <input type="number" class="form-control" placeholder="Rate" required>
+          <input type="number" class="form-control" placeholder="Rate*" required>
         </div>
         <div class="col-md-3">
           <input type="text" class="form-control" placeholder="Description">
@@ -50,7 +50,7 @@ document
       items: [],
       date_title: "Billing Date",
       due_date_title: "Due Date",
-      amount_paid: 0, // Initialize amount_paid
+      amount_paid: 0,
     };
 
     let amount_paid = 0;
@@ -73,35 +73,57 @@ document
 
     invoiceData.amount_paid = amount_paid;
 
-    const response = await fetch(
-      "https://billing-backend-ieyz.onrender.com/generate-invoice",
-      {
-        // Replace with your Render service URL
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(invoiceData),
-      }
-    );
+    try {
+      showLoader();
+      const response = await fetch(
+        "https://billing-backend-ieyz.onrender.com/generate-invoice",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(invoiceData),
+        }
+      );
 
-    const clientName = document.getElementById("client-name").value;
-    const clientPhone = document.getElementById("client-phone").value;
-    const today = new Date();
-    const formattedDate = `${today.getDate().toString().padStart(2, "0")}-${(
-      today.getMonth() + 1
-    )
-      .toString()
-      .padStart(2, "0")}-${today.getFullYear()}`;
-    const randomNumber = Math.floor(1000 + Math.random() * 90000);
+      const clientName = document.getElementById("client-name").value;
+      const clientPhone = document.getElementById("client-phone").value;
+      const today = new Date();
+      const formattedDate = `${today.getDate().toString().padStart(2, "0")}-${(
+        today.getMonth() + 1
+      )
+        .toString()
+        .padStart(2, "0")}-${today.getFullYear()}`;
+      const randomNumber = Math.floor(1000 + Math.random() * 90000);
 
-    const invoice = await response.blob();
-    const url = window.URL.createObjectURL(invoice);
-    const a = document.createElement("a");
-    a.style.display = "none";
-    a.href = url;
-    a.download = `${clientName}_${clientPhone}_${formattedDate}_${randomNumber}.pdf`; // Custom filename
-    document.body.appendChild(a);
-    a.click();
-    window.URL.revokeObjectURL(url);
+      const invoice = await response.blob();
+      const url = window.URL.createObjectURL(invoice);
+      const a = document.createElement("a");
+      a.style.display = "none";
+      a.href = url;
+      a.download = `${clientName}_${clientPhone}_${formattedDate}_${randomNumber}.pdf`; // Custom filename
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      showErrorToast();
+    } finally {
+      hideLoader();
+    }
   });
+
+function showLoader() {
+  document.getElementById("loader").style.display = "block";
+}
+
+function hideLoader() {
+  document.getElementById("loader").style.display = "none";
+}
+
+function showErrorToast() {
+  const toast = new bootstrap.Toast(document.getElementById("error-toast"));
+  toast.show();
+  setTimeout(() => {
+    toast.hide();
+  }, 5000);
+}
